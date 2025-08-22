e2a66825a9831b49c4a0932f4921eaa497e699ee
Henilsavaliya501 <henilsavaliya500@gmail.com>
2025-08-22 10:14:34 +0530

    Add files via upload

---
+++ great3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter number : ");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+    {
+    if(a>c)
+        {
+            printf("%d is greatter",a);
+        }
+    else
+        {
+             printf("%d is greatter",c);
+        }
+    }
+else
+{
+if(b>c) printf("%d is greater",b);
+else printf("%d is greater ",c);
+}
+}
